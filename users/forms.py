db1e0701b403804379089afb4d846026714f7b0e
chloejassur <C.M.Jassur2@newcastle.ac.uk>
2021-10-04 18:25:12 +0100

    Initial commit

---
+++ users/forms.py
@@ -0,0 +1,38 @@
+import re
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required, Email, ValidationError, Length, EqualTo
+
+
+def character_check(form, field):
+    excluded_chars="*?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Char {char} is not allowed."
+            )
+
+class RegisterForm(FlaskForm):
+    email = StringField(validators=[Required(), Email()])
+    firstname = StringField(validators=[Required(), character_check])
+    lastname = StringField(validators=[Required(), character_check])
+    phone = StringField(validators=[Required()])
+    password = PasswordField(validators=[Required(), Length(min=6, max=12, message="Password must be between 6 and 12 characters in length.")])
+    confirm_password = PasswordField(validators=[Required(),
+                                                 EqualTo('password', message='Both password fields must be equal.')])
+    pin_key = StringField(validators=[Required(), Length(min=32, message='Pin Key must be 32 characters long.')])
+
+    def validate_password(self, password):
+        p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[*?!%&/()=}{$@<>])')
+        if not p.match(self.password.data):
+            raise ValidationError("Password must contain atleast 1 digit, 1 lowercase letter, 1 upper case letter and 1 special character.")
+
+    def validate_phone(self, phone):
+        p = re.compile(r'\d\d\d\d-\d\d\d-\d\d\d\d')
+        if not p.fullmatch(self.phone.data):
+            raise ValidationError("Phone number must follow the pattern of XXXX-XXX-XXXX")
+
+    submit = SubmitField()
+
+
+

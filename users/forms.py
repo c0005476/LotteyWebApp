8a3c0ed8e7b0884c01993a8c8da425083e34210e
chloejassur <C.M.Jassur2@newcastle.ac.uk>
2021-10-04 19:00:38 +0100

    Created and wrote code for html pages for error handling. Wrote code for error handling in app.py lines 21 to 46.

--- users/forms.py
+++ users/forms.py
@@ -12,12 +12,15 @@ def character_check(form, field):
                 f"Char {char} is not allowed."
             )
 
+
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
     firstname = StringField(validators=[Required(), character_check])
     lastname = StringField(validators=[Required(), character_check])
     phone = StringField(validators=[Required()])
-    password = PasswordField(validators=[Required(), Length(min=6, max=12, message="Password must be between 6 and 12 characters in length.")])
+    password = PasswordField(validators=[Required(), Length(min=6, max=12,
+                                                            message="Password must be between 6 "
+                                                                    "and 12 characters in length.")])
     confirm_password = PasswordField(validators=[Required(),
                                                  EqualTo('password', message='Both password fields must be equal.')])
     pin_key = StringField(validators=[Required(), Length(min=32, max=32, message='Pin Key must be 32 characters long.')])
@@ -25,7 +28,8 @@ class RegisterForm(FlaskForm):
     def validate_password(self, password):
         p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[*?!%&/()=}{$@<>])')
         if not p.match(self.password.data):
-            raise ValidationError("Password must contain atleast 1 digit, 1 lowercase letter, 1 upper case letter and 1 special character.")
+            raise ValidationError("Password must contain at least 1 digit,"
+                                  " 1 lowercase letter, 1 upper case letter and 1 special character.")
 
     def validate_phone(self, phone):
         p = re.compile(r'\d\d\d\d-\d\d\d-\d\d\d\d')
